d21c2f8ac0a2fc9c00b19da0b8b92f88b9ab8623
tomer12901@gmail.com <tomer12901@gmail.com>
2020-09-20 02:32:47 +0300

    finished BFS algorithm

--- solver_tasks.cpp
+++ solver_tasks.cpp
@@ -1,5 +1,5 @@
 #include "solver_tasks.hpp"
-
+#define NO_PASSAGE_NODE -1
 
 namespace solver_tasks{
     /*Solver::Solver(const Problem& problem, const Solution& solution) noexcept : m_solution(solution), m_problem(problem){};
@@ -22,6 +22,30 @@ namespace solver_tasks{
         return m_row == pointNode.m_row && m_column == pointNode.m_column;
     }
 
+    std::string PointNode::operator>>(const PointNode& pointNode) const{
+        if(m_row + 1 == pointNode.getRow() && m_column == pointNode.getColumn()){
+            return "Right";
+        }
+        if(m_row - 1 == pointNode.getRow() && m_column == pointNode.getColumn()){
+            return "Left";
+        }
+        if(m_row == pointNode.getRow() && m_column + 1 == pointNode.getColumn()){
+            return "Down";
+        }
+        if(m_row == pointNode.getRow() && m_column - 1 == pointNode.getColumn()){
+            return "Up";
+        }
+        return "Error: can not reach with one direction";
+    }
+
+    unsigned int PointNode::getRow() const{
+        return m_row;
+    }
+
+    unsigned int PointNode::getColumn() const{
+        return m_column;
+    }
+
     MatrixGraphPath::MatrixGraphPath(const matrix::Matrix& matrixGraph, const PointNode& initialNode, const PointNode& goalNdoe) noexcept : m_matrixGraph(matrixGraph), m_initialNode(initialNode), m_goalNode(goalNdoe){};
 
     State<PointNode> MatrixGraphPath::getInitialState() const{
@@ -35,22 +59,22 @@ namespace solver_tasks{
     std::vector<State<PointNode>> MatrixGraphPath::getAllPossibleStates(const State<PointNode>& state) const{
         std::vector<State<PointNode>> possibleStates;
 
-        if(state.getNode().getRow() + 1 <= m_matrixGraph.matrixGetWidth() - 1){
+        if(state.getNode().getRow() + 1 <= m_matrixGraph.matrixGetWidth() - 1 && m_matrixGraph(state.getNode().getRow() + 1, state.getNode().getColumn()) != NO_PASSAGE_NODE){
             possibleStates.push_back(State<PointNode>(PointNode(state.getNode().getRow() + 1, state.getNode().getColumn()),
-             m_matrixGraph(state.getNode().getRow() + 1, state.getNode().getColumn())));
+            m_matrixGraph(state.getNode().getRow() + 1, state.getNode().getColumn())));
         }
 
-        if(state.getNode().getRow() - 1 >= 0) {
+        if(state.getNode().getRow() - 1 >= 0 && m_matrixGraph(state.getNode().getRow() - 1, state.getNode().getColumn()) != NO_PASSAGE_NODE) {
             possibleStates.push_back(State<PointNode>(PointNode(state.getNode().getRow() - 1, state.getNode().getColumn()),
             m_matrixGraph(state.getNode().getRow() - 1, state.getNode().getColumn())));
         }
 
-        if(state.getNode().getColumn() + 1 <= m_matrixGraph.matrixGetHeight() - 1){
+        if(state.getNode().getColumn() + 1 <= m_matrixGraph.matrixGetHeight() - 1 && m_matrixGraph(state.getNode().getRow(), state.getNode().getColumn() + 1) != NO_PASSAGE_NODE){
             possibleStates.push_back(State<PointNode>(PointNode(state.getNode().getRow(), state.getNode().getColumn() + 1),
-             m_matrixGraph(state.getNode().getRow(), state.getNode().getColumn() + 1)));
+            m_matrixGraph(state.getNode().getRow(), state.getNode().getColumn() + 1)));
         }
 
-        if(state.getNode().getColumn() - 1 >= 0){
+        if(state.getNode().getColumn() - 1 >= 0 && m_matrixGraph(state.getNode().getRow(), state.getNode().getColumn() - 1) != NO_PASSAGE_NODE){
             possibleStates.push_back(State<PointNode>(PointNode(state.getNode().getRow(), state.getNode().getColumn() - 1),
             m_matrixGraph(state.getNode().getRow(), state.getNode().getColumn() - 1)));
         }
--- solver_tasks.hpp
+++ solver_tasks.hpp
@@ -1,14 +1,10 @@
 #include "matrix/ClassMatrix.hpp"
 #include <string>
 #include <vector>
+#include <queue>
 namespace solver_tasks{
 
-class Solution{ 
-public:
-
-
-};
-
+/*
 class Result{
 
 };
@@ -26,22 +22,22 @@ private:
 
 public:
 
-   /**
+   
      * @brief Construct a new Solver object.
      * 
      * @param problem - problem object.
      * @param solution - way to solve the problem object.
      */
-    Solver(const Problem& problem, const Solution& solution) noexcept;
+   /* Solver(const Problem& problem, const Solution& solution) noexcept;
 
-    /**
+    
      * @brief The function solve the problem with he solution.
      * 
      * @return Result - the result of the solution of the problem.
      */
-    const Result& solve() const;
+  /*  const Result& solve() const;
 };
-
+*/
 
 template <class Node> class State{
 private:    
@@ -51,18 +47,77 @@ private:
     State<Node>* m_ptrCameFrom;
 
 public:
+    /**
+     * @brief Construct a new State object.
+     * 
+     * @param node - node of state.
+     * @param cost - cost of state.
+     */
     State(const Node& node, const unsigned int cost) noexcept : m_node(node), m_visited(false), m_cost(cost), m_ptrCameFrom(nullptr){};
 
+    /**
+     * @brief The function return node.
+     * 
+     * @return Node - node of state.
+     */
     Node getNode() const{
         return m_node;
     }
 
+    /**
+     * @brief The function return if state visited.
+     * 
+     * @return true - state visites.
+     * @return false - state does not visites.
+     */
+    bool getVisited() const{
+        return m_visited;
+    }
+
+    /**
+     * @brief The function return the previous state that came from.
+     * 
+     * @return State<Node> - previous state that came from.
+     */
+    State<Node> getCameFromState() const{
+        return *m_ptrCameFrom;
+    }
+
+    /**
+     * @brief The function retun cost of state.
+     * 
+     * @return unsigned int - cost of state.
+     */
+    unsigned int getCost() const{
+        return m_cost;
+    }
+
+    /**
+     * @brief The function set visited value.
+     * 
+     * @param visited - new visited value.
+     */
     void setVisited(const bool& visited){
         m_visited = visited;
     }
 
-    void setPrevNode(const State<Node>& state){
+    /**
+     * @brief The function set previous state that came from.
+     * 
+     * @param state - previous state that came from.
+     */
+    void setPrevState(const State<Node>& state){
         m_ptrCameFrom = &state;
+    }
+
+    /**
+     * @brief The function return the step to do to reach to another state(if its only one step).
+     * 
+     * @param state - step to reach.
+     * @return std::string - Up/Down/Right/Left.
+     */
+    std::string operator>>(const State<Node>& state) const{
+        return m_node >> state.getNode();
     }
 };
 
@@ -72,17 +127,44 @@ private:
     unsigned int m_column;
 
 public:
+    /**
+     * @brief Construct a new Point Node object
+     * 
+     * @param row - row of matrix point. 
+     * @param column - column of matrix point.
+     */
     PointNode(const unsigned int row, const unsigned int column) noexcept;
 
+    /**
+     * @brief The function checks if two points are equal.
+     * 
+     * @param pointNode - check if equal.
+     * @return true - equal.
+     * @return false - not equal.
+     */
     bool operator==(const PointNode& pointNode) const;
 
-    unsigned int getRow() const{
-        return m_row;
-    }
+     /**
+     * @brief The function return the step to do to reach to another state(if its only one step).
+     * 
+     * @param pointNode - matrix point to reach.
+     * @return std::string - Up/Down/Right/Left.
+     */
+    std::string operator>>(const PointNode& pointNode) const;
 
-    unsigned int getColumn() const{
-        return m_column;
-    }
+    /**
+     * @brief The function return the row of point.
+     * 
+     * @return unsigned int - row of point.
+     */
+    unsigned int getRow() const;
+
+    /**
+     * @brief The function return column of point.
+     * 
+     * @return unsigned int - column of point.
+     */
+    unsigned int getColumn() const;
 };
 
 
@@ -154,6 +236,104 @@ public:
      * @return std::vector<State<Node>> - vector of all possible states to reach from current state
      */
     virtual std::vector<State<PointNode>> getAllPossibleStates(const State<PointNode>& state) const;
+};
+
+template <class Node> class Solution{ 
+private:
+    std::vector<State<Node>> m_solutionPathStates;  
+    std::string m_algorithm;  
+public:
+    /**
+     * @brief Construct a new Solution object.
+     * 
+     * @param solutionPathStates - vector of states to solution path.
+     * @param algorithm - algorithm name.
+     */
+    Solution(const std::vector<State<Node>>& solutionPathStates, std::string algorithm){
+        m_solutionPathStates = solutionPathStates;
+        m_algorithm = algorithm;
+    }
+
+    /**
+     * @brief The function check if solution graph path is exist.
+     * 
+     * @return true - solution graph path is exist.
+     * @return false - solution graph path is not exist.
+     */
+    bool isSolutionExist() const{
+        return !m_solutionPathStates.empty();
+    }
+
+    /**
+     * @brief The function return solution path graph.
+     * 
+     * @return std::string - solution path graph.
+     */
+    std::string getSolution() const{
+        std::string solution;
+
+        unsigned int cost = 0;
+        for(State<Node> state : m_solutionPathStates){
+            cost += state.getCost();
+        }
+
+        solution += std::to_string(cost) + "," + m_algorithm;
+
+       for(int element; element < m_solutionPathStates.size() - 1; ++element){
+           solution += "," + m_solutionPathStates.at(element) >> m_solutionPathStates.at(element + 1);
+       }
+
+       return solution;
+    }
+
+};
+
+
+template <class Node> class Searcher{
+    /**
+     * @brief The function search the solution in searchable and return it.
+     * 
+     * @param searchable - object for search.
+     * @return Solution - solution of searching.
+     */
+    virtual Solution<Node> search(const Searchable<Node>& searchable) const = 0;
+};
+
+template <class Node> class breadthFirstSearch : public Searcher<Node>{
+    /**
+     * @brief The function search the solution in searchable and return it.
+     * 
+     * @param searchable - object for search.
+     * @return Solution - solution of searching.
+     */
+    virtual Solution<Node> search(const Searchable<Node>& searchable) const{
+        std::queue<State<Node>> statesQueue;
+        statesQueue.push(searchable.getInitialState());
+        searchable.getInitialState().setVisited(true);
+
+        while(!statesQueue.empty()){
+            State<Node> currentState = statesQueue.pop();
+            for(State<Node> state : searchable.getAllPossibleStates(currentState)){
+                if(!state.getVisited()){
+                    statesQueue.push(state);
+                    state.setVisited(true);
+                    state.setPrevState(currentState);
+                }
+            }
+        }
+        
+        std::vector<State<Node>> solutionPathStates;
+        for(State<Node> state = searchable.getGoalState(); state != nullptr; state = state.getCameFromState()){
+            solutionPathStates.push_back(state);    
+        }
+        solutionPathStates.reserve(solutionPathStates.begin(), solutionPathStates.end());
+
+        if(solutionPathStates.at(0).getNode() != searchable.getInitialState().getNode()){
+            solutionPathStates.clear();
+        }
+
+        return Solution<Node>(solutionPathStates, "BFS");
+    }
 };
 
 
