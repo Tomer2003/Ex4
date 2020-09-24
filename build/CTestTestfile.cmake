b4e2f721b0dae4388d813321b3009d4e1c03abbd
tomer12901@gmail.com <tomer12901@gmail.com>
2020-09-24 22:58:08 +0300

    delete unnecessary files

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tomer/Advanced Programmin 1/Ex4
-# Build directory: /home/tomer/Advanced Programmin 1/Ex4/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
